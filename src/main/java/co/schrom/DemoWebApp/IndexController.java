5393aae3227c0ea191ed57d65ca244f7fb138fd5
Moritz Schrom <moritz.schrom@schrom.co>
2021-06-21 23:53:08 +0200

    Added index page.

---
+++ src/main/java/co/schrom/DemoWebApp/IndexController.java
@@ -0,0 +1,15 @@
+package co.schrom.DemoWebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping
+    public String index() {
+        return "index";
+    }
+
+}
