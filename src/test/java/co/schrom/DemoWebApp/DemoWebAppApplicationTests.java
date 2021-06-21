cc699e6f9b566f0e36348c0d00cf24a233eac9f6
Moritz Schrom <moritz.schrom@schrom.co>
2021-06-21 23:43:13 +0200

    Initial commit

---
+++ src/test/java/co/schrom/DemoWebApp/DemoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.schrom.DemoWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
